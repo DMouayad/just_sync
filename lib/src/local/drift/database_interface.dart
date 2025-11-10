0b2e48db6b0bc50578b3a5946492fbf259532fbd
Mouayad <muayad.perun@outlook.com>
2025-11-10 07:59:10 +0200

    feat: remove SyncPointsTableMixin & PendingOpsTableMixin

--- lib/src/local/drift/database_interface.dart
+++ lib/src/local/drift/database_interface.dart
@@ -1,6 +1,7 @@
 import 'package:drift/drift.dart';
+import 'package:just_sync/src/models/query_spec.dart';
 
-import 'tables_mixin.dart';
+import 'utc_datetime_converter.dart';
 
 /// An interface implemented by user-defined Drift databases.
 ///
@@ -31,9 +32,27 @@ abstract class IDriftDatabase extends GeneratedDatabase {
 }
 
 /// Concrete table for storing synchronization points.
-/// Add this table to your `DriftDatabase`.
-class SyncPoints extends Table with SyncPointsTableMixin {}
+/// Add this table to your `@DriftDatabase`.
+class SyncPoints extends Table {
+  TextColumn get scopeName => text()();
+  TextColumn get scopeKeys => text()();
+  DateTimeColumn get lastSyncedAt => dateTime().map(UtcDateTimeConverter())();
+
+  @override
+  Set<Column> get primaryKey => {scopeName, scopeKeys};
+}
 
 /// Concrete table for storing pending offline operations.
-/// Add this table to your `DriftDatabase`.
-class PendingOps extends Table with PendingOpsTableMixin {}
+/// Add this table to your `@DriftDatabase`.
+class PendingOps extends Table {
+  TextColumn get id => text()();
+  TextColumn get scopeName => text()();
+  TextColumn get scopeKeys => text()();
+  IntColumn get opType => intEnum<PendingOpType>()();
+  TextColumn get entityId => text()();
+  TextColumn get payload => text().nullable()();
+  DateTimeColumn get updatedAt => dateTime().map(UtcDateTimeConverter())();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
